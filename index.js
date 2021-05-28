213c5e09e72c1bd0144f029be79c34b0fb92358b
Lyxdev <82411231+Lyxdev@users.noreply.github.com>
2021-05-28 19:55:11 +0200

    Reorganized Some Code

--- index.js
+++ index.js
@@ -36,17 +36,14 @@ client.on('message', message => {
     
     const args = message.content.slice(config.prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
-
     const command = client.commands.get(commandName)
-        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-    
-    if (!command) return
+    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
+    if (!command) return
     if (command.permissions) {
         if (message.channel.type === 'dm') {
             return;
         }
-
         const authorPerms = message.channel.permissionsFor(message.author);
         if (!authorPerms || !authorPerms.has(command.permissions)) {
             return;
@@ -55,27 +52,22 @@ client.on('message', message => {
 
     if (command.args && !args.length) {
         let reply = `You didnt provide any arguments, ${message.author}`;
-
         if (command.usage) {
             reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``
         }
-
         return message.channel.send(reply);
     }
 
     const { cooldowns } = client;
-
     if (!cooldowns.has(command.name)) {
         cooldowns.set(command.name, new Discord.Collection());
     }
-
     const now = Date.now()
     const timestamps = cooldowns.get(command.name);
     const cooldownAmount = (command.cooldown || 3) * 1000;
 
     if (timestamps.has(message.author.id)) {
         const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
         if (now < expirationTime) {
             const timeLeft = (expirationTime - now) / 1000;
             return message.reply(`Please wait ${timeLeft.toFixed(1)} more seconds to use this command again`);
@@ -87,7 +79,6 @@ client.on('message', message => {
 
     try {
         command.execute(message, args);
-
         console.log(`${message.author} used ${commandName} in ${message.channel}`)
     } catch (error) {
         console.log(error);
